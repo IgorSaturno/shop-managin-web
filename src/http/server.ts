6bc7b55357bc0b2a1585970f81dadf0006458414
igor nascimento saturnino <igornascimentosaturnino11@gmail.com>
2025-03-23 13:01:44 -0300

    add route of send-auth-link and register-store also add schema auth-links

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,23 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { registerStore } from "./routes/register-store";
+import { sendAuthLink } from "./routes/send-auth-link";
+import jwt from "@elysiajs/jwt";
+import { env } from "../env";
+import cookie from "@elysiajs/cookie";
 
-const app = new Elysia().get("/", () => {
-  return "Hello World";
-});
+const app = new Elysia()
+  .use(
+    jwt({
+      secret: env.JWT_SECRET_KEY,
+      schema: t.Object({
+        sub: t.String(),
+        storeId: t.Optional(t.String()),
+      }),
+    })
+  )
+  .use(cookie())
+  .use(registerStore)
+  .use(sendAuthLink);
 
 app.listen(3333, () => {
   console.log("🔥  HTTP server running!");
